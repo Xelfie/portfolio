2b051b9756f417a305872845d20c7482e42a5bbc
Xelfie <d.marine99@yahoo.co.uk>
2025-03-11 08:21:46 +0400

    WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,27 +9,42 @@ import SkillsAndHobbiesShowcase from "@/components/showcase/SkillsAndHobbiesShow
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-between p-24">
-      <div className="flex flex-col gap-8 px-2">
-        <div className="flex w-full items-center gap-8 dark:drop-shadow-[0_0_0.3rem_#ffffff70]">
-          <Image
-            className="relative rounded-full dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
-            src="/marine_dp2.png"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <p>
-            Hello! My name is Marine, but I like to go by Xelfie as my online
-            persona :D
-          </p>
+    <>
+      {/* <div className="h-screen w-screen">
+        <Image src="/cloud1.png" alt="Cloud image" layout="fill" />
+      </div> */}
+
+      <main className="flex min-h-screen flex-col justify-between p-24">
+        <div className="flex flex-col gap-8 px-2">
+          <div className="flex w-full items-center gap-8 dark:drop-shadow-[0_0_0.3rem_#ffffff70]">
+            <Image
+              src="/assets/cloud1.png"
+              alt="Cloud image"
+              // layout="fill"
+              width={180}
+              height={37}
+              // priority
+            />
+
+            <Image
+              className="relative rounded-full dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
+              src="/marine_dp2.png"
+              alt="Next.js Logo"
+              width={180}
+              height={180}
+              priority
+            />
+            <p>
+              Hello! My name is Marine, but I like to go by Xelfie as my online
+              persona :D
+            </p>
+          </div>
+
+          <MyWorkShowcase />
+
+          <SkillsAndHobbiesShowcase />
         </div>
-
-        <MyWorkShowcase />
-
-        <SkillsAndHobbiesShowcase />
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
---
+++ src/components/ui/SkillBox.tsx
@@ -0,0 +1,9 @@
+function SkillBox() {
+  return (
+    <div className="w-1/2 rounded-lg border-2 p-2 dark:drop-shadow-[0_0_0.3rem_#ffffff70]">
+      SkillBox
+    </div>
+  );
+}
+
+export default SkillBox;
